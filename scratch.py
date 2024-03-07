45d1c69519ad6fa92b76002aaf1ea42bf37034d4
Isaiah <32691164+IMMachinations@users.noreply.github.com>
2024-03-06 20:51:43 -0600

    Debugged second paper experiment 1

--- scratch.py
+++ scratch.py
@@ -14,7 +14,7 @@ from jaxtyping import Float
 from typing import Optional, Callable, Union
 from tqdm.auto import tqdm
 from dataclasses import dataclass
-from transformer_lens import HookedTransformer
+from transformer_lens import HookedTransformer, hook_points
 from typing import List, Tuple
 import random
 
@@ -60,21 +60,21 @@ def mix_contexts_and_query(contexts : List[Tuple[str,str]], query: str, function
         token_list.append(model.to_single_token(seperator_token))
     return token_list + [model.to_single_token(query), model.to_single_token(function_token)]
 def mix_multitoken_contexts_and_query(contexts : List[Tuple[str,str]], query: str, function_token: str = "→", seperator_token: str = None, model: HookedTransformer = model) -> List[int]:
-    function_token_list = model.to_tokens(function_token,prepend_bos=False).tolist()
+    function_token_list = model.to_tokens(function_token,prepend_bos=False).tolist()[0]
     token_list = [0]
     has_seperator = seperator_token is not None
     if(has_seperator):
-        seperator_tokens = model.to_tokens(seperator_token,prepend_bos=False).tolist()
+        seperator_tokens = model.to_tokens(seperator_token,prepend_bos=False).tolist()[0]
 
     for context in contexts:
-        token_list += model.to_tokens(context[0], prepend_bos=False).tolist()
+        token_list += model.to_tokens(context[0], prepend_bos=False).tolist()[0]
         token_list += function_token_list
-        token_list += model.to_tokens(context[1], prepend_bos=False).tolist()
+        token_list += model.to_tokens(context[1], prepend_bos=False).tolist()[0]
         if(has_seperator):
             token_list += seperator_tokens
     if(has_seperator):
         token_list += seperator_tokens
-    return token_list + model.to_tokens(query,prepend_bos=False).tolist() + function_token_list
+    return token_list + model.to_tokens(query,prepend_bos=False).tolist()[0] + function_token_list
 
 # %%
 low_to_caps_strings = [construct_context(pair, arrow) for pair in low_to_caps]
@@ -224,12 +224,13 @@ substitute_task_layer_13 = substitute_task(letter_to_caps, letter_to_low, 13, ar
 substitute_task_layer_14 = substitute_task(letter_to_caps, letter_to_low, 14, arrow, model, num_contexts=2048, len_contexts=6)
 # %%
 ### PART 2: Function Vectors in LLMS
-def generate_mean_activation(contexts: List[Tuple[str,str]], query: str, function_token: str, model: HookedTransformer = model, num_contexts: int = 1024, len_contexts: int = 4) -> Float:
+def generate_mean_activation(contexts: List[Tuple[str,str]], function_token: str, seperator_token: str = ",", model: HookedTransformer = model, num_contexts: int = 1024, len_contexts: int = 4) -> Float:
     
     shuffled_context = contexts.copy()  
     num_total = 0
     activations = t.zeros((model.cfg.n_layers, model.cfg.n_heads, model.cfg.d_model))
-    model.cfg.use_attn_results = True
+    previous_attn_result_value = model.cfg.use_attn_result 
+    model.cfg.use_attn_result = True
     for _ in tqdm(range(num_contexts)):
         num_total += 1
         random.shuffle(shuffled_context)
@@ -237,29 +238,31 @@ def generate_mean_activation(contexts: List[Tuple[str,str]], query: str, functio
         current_query = shuffled_context[len_contexts]
         current_answer = shuffled_context[len_contexts][1]
 
-        normal_tokens = t.tensor(mix_contexts_and_query(current_context, current_query[0], function_token, model))
+        normal_tokens = mix_multitoken_contexts_and_query(current_context, current_query[0], function_token, seperator_token, model)
+        normal_tokens = t.tensor(normal_tokens)
         _, normal_cache = model.run_with_cache(normal_tokens)
         for i in range(model.cfg.n_layers):
-            activations[i,:,:] += normal_cache[f"blocks.{i}.attn.attn_results"][0,-1,:,:] # maybe [0,:,-1,:]
-    
+            activations[i,:,:] += normal_cache[f"blocks.{i}.attn.hook_result"][0,-1,:,:] # maybe [0,:,-1,:]
+    model.cfg.use_attn_result = previous_attn_result_value
     return activations/num_contexts
 
 def gather_head_activations_to_layers(mean_head_activations: Tensor) -> Tensor:
-    return mean_head_activations.mean(1)
+    return mean_head_activations.sum(1)
 
-def layer_addition_hook(hook_value: Tensor, hook: HookPoint, vector: Tensor) -> Tensor:
+def layer_addition_hook(hook_value: Tensor, hook: hook_points.HookPoint, vector: Tensor) -> Tensor:
     hook_value[0,-1,:] = hook_value[0,-1,:] + vector
     return hook_value 
-    
+
+def logits_to_next_token(logits: Tensor, model: HookedTransformer = model) -> int:
+    return model.to_string(t.argmax(logits[0,-1,:]))
 
 def apply_layered_vectors_to_zero_shot(layered_vectors: Tensor, contexts: List[Tuple[str,str]], function_token: str, model: HookedTransformer = model) -> Float:
     layer_sums = [0 * model.cfg.n_layers]
     hook_functions = [lambda hook_value, hook : layer_addition_hook(hook_value, hook, vector) for vector in layered_vectors]
     for context in contexts:
-        
         tokens = t.tensor([0, model.to_single_token(context[0]), model.to_single_token(function_token)])
         for i in range(model.cfg.n_layers):
-            logits = model.run_with_hooks(tokens, fwd_hooks=[(f"blocks.{i}.attn.attn_results", hook_functions[i])])
+            logits = model.run_with_hooks(tokens, fwd_hooks=[(f"blocks.{i}.hook_attn_out", hook_functions[i])])
             if(logits_to_next_token(logits) == model.to_single_token(context[1])):
                 layer_sums[i] += 1
 
@@ -270,3 +273,12 @@ def apply_layered_vectors_to_zero_shot(layered_vectors: Tensor, contexts: List[T
 
 def identify_probability_of_token(logits: Tensor, token: str, model: HookedTransformer = model) -> Float:
     return t.nn.functional.softmax(logits[0,-1,:])[model.to_single_token(token)]
+
+
+# %%
+mean_head_activations = generate_mean_activation(letter_to_caps, arrow, num_contexts=2048, len_contexts=6)
+# %%
+mean_layer_activations = gather_head_activations_to_layers(mean_head_activations)
+# %%
+layered_accuracy = apply_layered_vectors_to_zero_shot(mean_layer_activations, letter_to_caps, arrow, model)
+# %%
